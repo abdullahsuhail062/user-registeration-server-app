974df1aac12d5b240a5fb06c316258c30dc6a81d
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-17 13:31:09 +0500

    update

--- src/app/pages/api/register.ts
+++ src/app/pages/api/register.ts
@@ -1,7 +1,7 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://user-registeration-application.vercel.app');  // Allow requests from Angular app
+    res.setHeader('Access-Control-Allow-Origin', 'https://user-registeration-application-79zv722q0-suhailumars-projects.vercel.app');  // Allow requests from Angular app
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
